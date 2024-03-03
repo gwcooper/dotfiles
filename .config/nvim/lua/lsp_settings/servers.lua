304e54064e8663ceba28b6d403017edb3abf0288
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-03-03 19:03:37 +0200

    nvim: abstract lsp servers to lsp_settings dir

---
+++ .config/nvim/lua/lsp_settings/servers.lua
@@ -0,0 +1,16 @@
+return {
+  -- python
+  "pyright",
+  -- terraform
+  "terraformls",
+  -- lua
+  "lua_ls",
+  -- groovy
+  "groovyls",
+  -- elixir
+  "elixirls",
+  -- bash
+  "bashls",
+  -- markdown
+  "marksman",
+}
