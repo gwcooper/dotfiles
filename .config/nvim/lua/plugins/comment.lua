ddb394740755717193c621e4feeccbc77d726c4b
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-03-03 19:32:26 +0200

    nvim: revert to old comment packag

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,16 +1,33 @@
-return {
-  "echasnovski/mini.comment",
-  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-  event = "VeryLazy",
-  opts = {
-    options = {
-      custom_commentstring = function()
-        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-      end,
+local M = {
+  "numToStr/Comment.nvim",
+  lazy = false,
+  dependencies = {
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      event = "VeryLazy",
     },
   },
-  keys = {
-    { "<leader>/", "<Plug>(comment_toggle_linewise_current)", "Comment line" },
-    { "<leader>/", "<Plug>(comment_toggle_linewise_visual)", "Comment line", mode = "v" },
-  },
 }
+
+function M.config()
+  local wk = require("which-key")
+  wk.register({
+    ["<leader>/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment" },
+  })
+
+  wk.register({
+    ["<leader>/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment", mode = "v" },
+  })
+
+  vim.g.skip_ts_context_commentstring_module = true
+  ---@diagnostic disable: missing-fields
+  require("ts_context_commentstring").setup({
+    enable_autocmd = false,
+  })
+
+  require("Comment").setup({
+    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+  })
+end
+
+return M
