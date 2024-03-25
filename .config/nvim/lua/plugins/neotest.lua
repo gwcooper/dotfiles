877c64046283cbb239dfc05e876f42332b6f75af
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-03-25 23:04:23 +0200

    nvim: update neotest config

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,29 +1,26 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
+    "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
     "nvim-neotest/neotest-python",
-    -- "nvim-neotest/neotest-plenary",
   },
   config = function()
     require("neotest").setup({
       adapters = {
         require("neotest-python")({
           dap = { JustMyCode = false },
-          }),
-        require("neotest-plenary"),
-        require("neotest-vim-test")({
-          ignore_file_types = { "python", "vim", "lua" },
         }),
       },
     })
   end,
   keys = {
     { "<leader>dtt", ":lua require'neotest'.run.run({strategy = 'dap'})<cr>", desc = "test" },
-    { "<leader>dts", ":lua require'neotest'.run.stop()<cr>",                  desc = "stop test" },
-    { "<leader>dta", ":lua require'neotest'.run.attach()<cr>",                desc = "attach test" },
+    { "<leader>dts", ":lua require'neotest'.run.stop()<cr>", desc = "stop test" },
+    { "<leader>dta", ":lua require'neotest'.run.attach()<cr>", desc = "attach test" },
     { "<leader>dtf", ":lua require'neotest'.run.run(vim.fn.expand('%'))<cr>", desc = "test file" },
-    { "<leader>dts", ":lua require'neotest'.summary.toggle()<cr>",            desc = "test summary" },
+    { "<leader>dts", ":lua require'neotest'.summary.toggle()<cr>", desc = "test summary" },
   },
 }
