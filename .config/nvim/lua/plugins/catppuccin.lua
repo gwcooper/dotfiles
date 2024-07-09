eeb00bbf9eef700c11787664b3ef66d679711886
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-07-09 16:07:37 +0200

    nvim: update theme settings

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,13 +1,43 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "macchiato",
-			transparent_background = true,
-		})
-
-		vim.cmd("colorscheme catppuccin")
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    term_colors = true,
+    transparent_background = true,
+  },
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    treesitter = true,
+    harpoon = true,
+    telescope = true,
+    mason = true,
+    noice = true,
+    notify = true,
+    which_key = true,
+    fidget = true,
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+      },
+      inlay_hints = {
+        background = true,
+      },
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin-macchiato")
+  end,
 }
