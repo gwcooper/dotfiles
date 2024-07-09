4df221ddb2384e4fee03f75250702d6e6cbe4e67
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-07-09 16:03:18 +0200

    nvim: update ruff lsp and lsp server settings

--- .config/nvim/lua/lsp_settings/servers.lua
+++ .config/nvim/lua/lsp_settings/servers.lua
@@ -2,14 +2,8 @@ return {
   -- python
   "basedpyright",
   "ruff_lsp",
-  -- terraform
-  "terraformls",
   -- lua
   "lua_ls",
-  -- groovy
-  "groovyls",
-  -- elixir
-  "elixirls",
   -- bash
   "bashls",
   -- markdown
