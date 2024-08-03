df869fa0139c759b10d30c0ea8bfd536a541a5bc
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-08-03 23:09:00 +0200

    nvim: notify update to filter messages

--- .config/nvim/lua/plugins/nvim-notify.lua
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -24,6 +24,16 @@ return {
     end,
   },
   init = function()
-    vim.notify = require("notify")
+    local banned_messages = { "No information available" }
+
+    vim.notify = function(msg, ...)
+      for _, banned in ipairs(banned_messages) do
+        if msg == banned then
+          return
+        end
+      end
+
+      require("notify")(msg, ...)
+    end
   end,
 }
