d5560ad906c46b3c5dceb2f46ed455758208c79a
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-12-11 09:27:39 +0200

    Nvim: rm archive folder

--- .config/nvim/lua/archive/groovyls.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- cmd = { "bash", "/Users/george/.local/share/nvim/mason/bin/groovy-language-server" },
-  cmd = {
-    "java",
-    "-jar",
-    "/Users/george/.local/share/nvim/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar",
-  },
-}
