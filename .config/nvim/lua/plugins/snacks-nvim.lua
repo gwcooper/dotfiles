6ad1fe1841d305081860a9b213db0a049b1c8b05
George Cooper <5212955+gwcooper@users.noreply.github.com>
2024-12-29 20:49:37 +0200

    nvim: revert to neoscroll and update snacks.nvim

--- .config/nvim/lua/plugins/snacks-nvim.lua
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -1,5 +1,6 @@
 return {
   "folke/snacks.nvim",
+  version = "*",
   dependencies = { "echasnovski/mini.icons", version = false },
   priority = 1000,
   lazy = false,
@@ -7,7 +8,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    dim = { enabled = true }, --TODO: configure
+    dim = { enabled = true },
     notifier = {
       enabled = true,
       timeout = 3000,
@@ -24,28 +25,27 @@ return {
     indent = { enabled = true },
     input = { enabled = true },
     scope = { enabled = true },
-    scroll = { enabled = true },
-    zen = { enabled = true },
+    zen = { enabled = true, toggles = { dim = true, statuscolumn = false, line_number = true } },
   },
   -- stylua: ignore
   keys = {
+    {"<leader>t", group = "toggles"},
     { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
     { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-    { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+    { "<leader>Nn",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-    { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
     { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
     { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
     { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
     { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
-    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    { "<leader>tn", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
     { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
     { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     {
-      "<leader>N",
+      "<leader>NN",
       desc = "Neovim News",
       function()
         Snacks.win({
@@ -77,18 +77,18 @@ return {
         vim.print = _G.dd -- Override print to use snacks for `:=` command
 
         -- Create some toggle mappings
-        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-        Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
+        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>ts")
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>tw")
+        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL")
+        Snacks.toggle.diagnostics():map("<leader>td")
+        Snacks.toggle.line_number():map("<leader>tl")
         Snacks.toggle
           .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-          :map("<leader>uc")
-        Snacks.toggle.treesitter():map("<leader>uT")
-        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-        Snacks.toggle.inlay_hints():map("<leader>uh")
-        -- Snacks.toggle.option("zenmode", { off = Snacks.zen.zen(false), on = Snacks.zen.zen(false), name = "ZenMode" }):map("<leader>uz") --TODO: update once this is available
+          :map("<leader>tc")
+        Snacks.toggle.treesitter():map("<leader>tT")
+        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>tb")
+        Snacks.toggle.inlay_hints():map("<leader>th")
+        Snacks.toggle.zen():map("<leader>tz")
       end,
     })
   end,
